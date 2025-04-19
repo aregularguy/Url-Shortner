6e20f4a92180e881c6b925bdf969c0d5637a91d8
Arshad Bagwan <v-arshadb@zillowgroup.com>
2025-04-19 14:14:36 +0530

    initial project setup commit

---
+++ src/main/java/com/url/UrlShortnerApIsApplication.java
@@ -0,0 +1,13 @@
+package com.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortnerApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShortnerApIsApplication.class, args);
+	}
+
+}
